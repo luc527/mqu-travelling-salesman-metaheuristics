91b49286918d9746f8285ea5ca17576bb31e7fab
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-06-25 20:51:44 -0300

    simple local search, brute force

--- common.py
+++ common.py
@@ -1,5 +1,12 @@
+from typing import Tuple
 import tsplib95 as tsplib
+import networkx as nx
+import graphviz
 import sys
+import random
+from itertools import permutations
+
+# let's get instances from http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/tsp/
 
 def square_matrix(n, x=None):
     m = [[] for _ in range(n)]
@@ -15,10 +22,86 @@ def print_matrix(m):
 
 def parse_instance(filename):
     problem = tsplib.load(filename)
-    return problem.edge_weights
+    g = problem.get_graph()
+    return g
 
-distmatrix = parse_instance(sys.argv[1])
+def evaluate(graph, solution):
+    s = 0
+    n = len(solution)
+    for i in range(n):
+        v = solution[i]
+        w = solution[(i+1)%n]
+        s += graph.edges[v, w]['weight']
+    return s
 
-print_matrix(distmatrix)
+def brute_force(graph: nx.Graph) -> Tuple[float, list]:
+    all_cycles = map(list, permutations(range(graph.number_of_nodes())))
+    return min(map(lambda sol: (evaluate(graph, sol), sol), all_cycles))
 
-# let's get instances from http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/tsp/
+def neighborhood(solution):
+    n = len(solution)
+    neighborhood = []
+    for i in range(n):
+        j = (i + 1) % n
+        neighbour = list(solution)
+        neighbour[i], neighbour[j] = neighbour[j], neighbour[i]
+        neighborhood.append(neighbour)
+    return neighborhood
+
+def simple_local_search_impl(graph, solution):
+    weight = evaluate(graph, solution)
+    iters = 10
+    for _ in range(iters):
+        nhood = list(map(lambda sol: (evaluate(graph, sol), sol), neighborhood(solution)))
+        sub = min(nhood)
+        if sub[0] == weight:
+            break
+        elif sub[0] < weight:
+            weight = sub[0]
+            solution = sub[1]
+    return (weight, solution)
+
+
+def simple_local_search(graph: nx.Graph) -> Tuple[float, list]:
+    initial = [i for i in range(graph.number_of_nodes())]
+    random.shuffle(initial)
+    return simple_local_search_impl(graph, initial)
+
+
+def output_image(path, graph, cycle):
+    lastidx = -1
+    for i, c in enumerate(path):
+        if c == '/':
+            lastidx = i
+    path = path[lastidx+1:]
+
+    verts = graph.number_of_nodes()
+
+    cycle_edges = list(zip(cycle, [cycle[-1]] + cycle[:-1]))
+
+    viz = graphviz.Graph(path)
+    # viz.attr('node', {'shape': 'point'})
+    viz.attr('edge', {'fontsize': '8'})
+    for a in range(verts):
+        viz.node(str(a))
+    for b in range(verts):
+        for a in range(b):
+            label = str(graph.edges[a,b]['weight'])
+            attr = {}
+            if (a, b) in cycle_edges or (b, a) in cycle_edges:
+                attr['color'] = 'red'
+                attr['penwidth'] = '3'
+            viz.edge(str(a), str(b), label, attr)
+    dir = 'images'
+    print(f'Rendered graph on {path}, see {viz.render(directory=dir)}') 
+
+path = sys.argv[1]
+
+graph = parse_instance(path)
+
+(weight, solution) = simple_local_search(graph)
+print(weight, solution)
+
+print(brute_force(graph))
+
+output_image(path, graph, solution)
