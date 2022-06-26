d0407101870debac296368c4f3615593675ba3f6
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2022-06-25 21:28:13 -0300

    simplify code

--- common.py
+++ common.py
@@ -8,66 +8,6 @@ from itertools import permutations
 
 # let's get instances from http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/tsp/
 
-def square_matrix(n, x=None):
-    m = [[] for _ in range(n)]
-    for i in range(n):
-        m[i] = [x for _ in range(n)]
-    return m
-
-def print_matrix(m):
-    for i in range(0, len(m)):
-        for j in range(0, len(m[i])):
-            print(m[i][j], end='\t')
-        print()
-
-def parse_instance(filename):
-    problem = tsplib.load(filename)
-    g = problem.get_graph()
-    return g
-
-def evaluate(graph, solution):
-    s = 0
-    n = len(solution)
-    for i in range(n):
-        v = solution[i]
-        w = solution[(i+1)%n]
-        s += graph.edges[v, w]['weight']
-    return s
-
-def brute_force(graph: nx.Graph) -> Tuple[float, list]:
-    all_cycles = map(list, permutations(range(graph.number_of_nodes())))
-    return min(map(lambda sol: (evaluate(graph, sol), sol), all_cycles))
-
-def neighborhood(solution):
-    n = len(solution)
-    neighborhood = []
-    for i in range(n):
-        j = (i + 1) % n
-        neighbour = list(solution)
-        neighbour[i], neighbour[j] = neighbour[j], neighbour[i]
-        neighborhood.append(neighbour)
-    return neighborhood
-
-def simple_local_search_impl(graph, solution):
-    weight = evaluate(graph, solution)
-    iters = 10
-    for _ in range(iters):
-        nhood = list(map(lambda sol: (evaluate(graph, sol), sol), neighborhood(solution)))
-        sub = min(nhood)
-        if sub[0] == weight:
-            break
-        elif sub[0] < weight:
-            weight = sub[0]
-            solution = sub[1]
-    return (weight, solution)
-
-
-def simple_local_search(graph: nx.Graph) -> Tuple[float, list]:
-    initial = [i for i in range(graph.number_of_nodes())]
-    random.shuffle(initial)
-    return simple_local_search_impl(graph, initial)
-
-
 def output_image(path, graph, cycle):
     lastidx = -1
     for i, c in enumerate(path):
@@ -94,6 +34,49 @@ def output_image(path, graph, cycle):
             viz.edge(str(a), str(b), label, attr)
     dir = 'images'
     print(f'Rendered graph on {path}, see {viz.render(directory=dir)}') 
+
+def parse_instance(filename):
+    return tsplib.load(filename).get_graph()
+
+def brute_force(g: nx.Graph) -> Tuple[float, list]:
+    all_cycles = map(list, permutations(range(g.number_of_nodes())))
+    return min(map(lambda s: (evaluate(g, s), s), all_cycles))
+
+def evaluate(graph, solution):
+    s = 0
+    n = len(solution)
+    for i in range(n):
+        v = solution[i]
+        w = solution[(i+1)%n]
+        s += graph.edges[v, w]['weight']
+    return s
+
+def neighborhood(solution):
+    n = len(solution)
+    neighborhood = []
+    for i in range(n):
+        j = (i + 1) % n
+        nb = list(solution)
+        nb[i], nb[j] = nb[j], nb[i]
+        neighborhood.append(nb)
+    return neighborhood
+
+def simple_local_search(graph: nx.Graph) -> Tuple[float, list]:
+    # Initial solution: random cycle
+    solution = [i for i in range(graph.number_of_nodes())]
+    random.shuffle(solution)
+    weight = evaluate(graph, solution)
+
+    # Stop criteria: # of iterations
+    for _ in range(1000):
+        best = min(map( \
+            lambda sol: (evaluate(graph, sol), sol), \
+            neighborhood(solution) \
+        ))
+        if best[0] < weight:
+            (weight, solution) = best
+    return (weight, solution)
+
 
 path = sys.argv[1]
 
